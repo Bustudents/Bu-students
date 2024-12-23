35d7f4138fba8a2afe8a9647609083b9c308d04b
unknown <butudents19@gmail.com>
2024-12-23 15:28:10 +0300

    change

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -156,7 +156,7 @@ const SignUpPage = () => {
               <option value="">Select Gender</option>
               <option value="Male">Male</option>
               <option value="Female">Female</option>
-              <option value="Other">leena is gay</option>
+              
             </select>
           </div>
           <div>
