fe7e967cca9a8a435424df5128d0b443b78ad0ea
unknown <butudents19@gmail.com>
2025-01-01 22:11:32 +0300

    vg

--- app/homepage/comonents/test.jsx
+++ app/homepage/comonents/test.jsx
@@ -31,7 +31,7 @@ function Section({ id, title, text, isEarlyVisible }) {
         scrollSnapType: "y mandatory",
         height: "215vh", // Reduced height for less spacing
       }}
-      className="flex flex-col 2xl:mt-[-600px] xs:mt-[-800] justify-center ml-10 items-center relative overflow-hidden bg-gradient-to-b"
+      className="flex flex-col xs:mt-[-700px] 2xl:mt-[-600px] h-min justify-center ml-10 items-center relative overflow-hidden bg-gradient-to-b"
     >
       <div className="flex flex-col lg:flex-row justify-center items-center text-center lg:text-left">
         {/* Image with 3D effect */}
